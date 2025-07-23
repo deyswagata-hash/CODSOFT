fb8b70542cd2234376b09ee5c5151f2566ac4bfb
deyswagata-hash <deyswagata58@gmail.com>
2025-07-23 20:42:42 +0530

    Add files via upload

---
+++ Soul Mandalas/script.js
@@ -0,0 +1,4 @@
+document.getElementById("viewgallerybtn").addEventListener("click",function(){
+    documentgotElementById("gallery").classList.remove("hidden");
+    this.style.display="none";
+});
